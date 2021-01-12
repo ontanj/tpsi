dd9ae3307b3a91cf8612a4f86f8c29110da1c1ec
Anton Jeppsson <anton@ontanj.se>
2021-01-12 17:30:05 +0100

    Embed AHESetting in FHESetting, improve docs

--- setting.go
+++ setting.go
@@ -1,28 +1,40 @@
 package tpsi
 
 type AHE_setting interface {
+    // threshold value
     Threshold() int
     
+    // numer of parties
     Parties() int
     
+    // ahe cryptosystem
     AHE_cryptosystem() AHE_Cryptosystem
 
+    // used by central party to send a value to all
     Distribute(interface{})
     
+    // used by outer parties to send to central party
     Send(interface{})
 
+    // used by central party to send
+    // a message to given party
     SendTo(int, interface{})
     
+    // for central to await messages from all
+    // and get them (ordered) in a slice
     ReceiveAll() []interface{}
 
+    // receive a message from central party
     Receive() interface{}
 
+    // true if this party is central
     IsCentral() bool
 }
 
 type FHE_setting interface {
     AHE_setting
 
+    // fhe cryptosystem
     FHE_cryptosystem() FHE_Cryptosystem
 }
 
@@ -77,19 +89,8 @@ func (s AHESetting) IsCentral() bool {
 }
 
 type FHESetting struct {
+    AHESetting
     cs FHE_Cryptosystem
-    n int // number of participants
-    T int // threshold
-    channels []chan interface{}
-    channel chan interface{}
-}
-
-func (s FHESetting) Threshold() int {
-    return s.T
-}
-
-func (s FHESetting) Parties() int {
-    return s.n
 }
 
 func (s FHESetting) AHE_cryptosystem() AHE_Cryptosystem {
@@ -98,34 +99,4 @@ func (s FHESetting) AHE_cryptosystem() AHE_Cryptosystem {
 
 func (s FHESetting) FHE_cryptosystem() FHE_Cryptosystem {
     return s.cs
-}
-
-func (s FHESetting) Distribute(any interface{}) {
-    for _, ch := range s.channels {
-        ch <- any
-    }
-}
-
-func (s FHESetting) Send(any interface{}) {
-    s.channel <- any
-}
-
-func (s FHESetting) SendTo(i int, any interface{}) {
-    s.channels[i] <- any
-}
-
-func (s FHESetting) ReceiveAll() []interface{} {
-    sl := make([]interface{}, s.n-1)
-    for i, ch := range s.channels {
-        sl[i] = <-ch
-    }
-    return sl
-}
-
-func (s FHESetting) Receive() interface{} {
-    return <-s.channel
-}
-
-func (s FHESetting) IsCentral() bool {
-    return s.channels != nil
 }
